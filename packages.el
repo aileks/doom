79ee2d1409e12deb3c7c44b4e34cc332d50d52b4
Kieran Pedrick <kieran@aileks.dev>
2026-09-18 10:07:23 -0400

    remove unused package

--- packages.el
+++ packages.el
@@ -3,8 +3,6 @@
 (package! lsp-java)
 (package! dap-mode)
 (package! lsp-treemacs) 
-(package! cinder-grove
-  :recipe (:host github :repo "aileks/cinder-grove.el"))
 (package! cinder-muted
   :recipe (:host github :repo "aileks/cinder-muted"
            :files ("emacs/cinder-muted-theme.el")))
