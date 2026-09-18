69c96143fa0fa41762f1d913fe7e37bed6b4ecac
Kieran Pedrick <kieran@aileks.dev>
2026-09-17 21:31:35 -0400

    fix packages.el

--- packages.el
+++ packages.el
@@ -4,8 +4,7 @@
 (package! dap-mode)
 (package! lsp-treemacs) 
 (package! cinder-grove
-  :recipe (:host github :repo "aileks/cinder-grove.el")
-  :pin "d7555f75e3304f614ca40c5d9791824fcb036986")
+  :recipe (:host github :repo "aileks/cinder-grove.el"))
 (package! cinder-muted
   :recipe (:host github :repo "aileks/cinder-muted"
            :files ("emacs/cinder-muted-theme.el")))
